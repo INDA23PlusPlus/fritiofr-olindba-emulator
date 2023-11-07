1cad33583a89647c3c9e1a3310c85cba2b615ee0
Fritiof Rusck <fritiof@rusck.se>
2023-11-07 18:05:59 +0100

    chore: remove println

--- examples/is_prime.rs
+++ examples/is_prime.rs
@@ -2,7 +2,5 @@ const CODE: &'static str = include_str!("is_prime.asm");
 
 fn main() {
     let code = emulator::lexer::Lexer::new(CODE).generate();
-    println!("{:?}", code);
-    println!("{:?}", CODE);
     emulator::emulator::run(code)
 }

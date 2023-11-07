dc3e745288c5fceff984eab538e7a86782078d24
ok26 <oliver.lindback@gmail.com>
2023-11-07 17:22:42 +0100

    feat: add ir compiler

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod consts;
 pub mod emulator;
-
+pub mod lexer;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let mut lexer = emulator::lexer::Lexer::new(".prime_loop\ndiv cx bx dx\ncnd ax 0\njeq .prime_loop_end\nadd cx 1 cx\ncnd cx bx\njne .prime_loop\n.prime_loop_end");
+    let ret = lexer.generate();
+    println!("{:?}", ret);
+    println!("{}", ret.len());
 }

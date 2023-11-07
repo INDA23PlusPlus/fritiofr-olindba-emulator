fae358dc39061b23b4edef0ff086d907e9eece4e
Fritiof Rusck <fritiof@rusck.se>
2023-11-07 15:34:06 +0100

    feat: add all instructions to consts (#3)

--- src/consts.rs
+++ src/consts.rs
@@ -4,3 +4,17 @@ pub const CX: u8 = 3;
 pub const DX: u8 = 4;
 pub const EX: u8 = 5;
 pub const FX: u8 = 6;
+
+pub const ADD: u8 = 1;
+pub const SUB: u8 = 2;
+pub const MUL: u8 = 3;
+pub const DIV: u8 = 4;
+
+pub const CND: u8 = 5;
+pub const JMP: u8 = 6;
+pub const JEQ: u8 = 7;
+pub const JNE: u8 = 8;
+pub const JGT: u8 = 9;
+pub const JLT: u8 = 10;
+pub const JGE: u8 = 11;
+pub const JLE: u8 = 12;
